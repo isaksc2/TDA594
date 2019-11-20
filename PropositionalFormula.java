e2d52de43c13ba6b2e36e2cf07011bee99fe36be
isaksc2 <isaksc@student.chalmers.se>
2019-11-20 14:31:35 +0100

    finished model props

--- PropositionalFormula.java
+++ PropositionalFormula.java
@@ -18,6 +18,7 @@ public class PropositionalFormula {
 	
 	boolean Targeting;
 	int nbrVirtualGuns;
+	// VirtualGuns = nbrVirtualGuns > 0
 	boolean VirtualGuns;
 	boolean GuessFactorGun;
 	boolean HeadOnTargeting;
@@ -47,6 +48,41 @@ public class PropositionalFormula {
 	boolean SaveTargetingData;
 	boolean SaveBulletSynchronizedSnapshots;
 	boolean SaveMovementData;
-	boolean SaveWaveSurfing;
+	boolean SaveWaveSurfingStats;
 	boolean SaveGuessFactor;
+
+	private boolean implies(boolean x, boolean y) {
+	    return ((x && y)||(!x && y)||(!x && !y))
+    }
+
+    private boolean iff(boolean x, boolean y) {
+	    return ((x && y) || (!x && !y))
+    }
+
+    boolean formula =
+        // dependencies shown in the model, indentation means 1 level down
+        implies(Movement, TDA594) &&
+            iff(Movement, WaveSurfing || PrecisPrediction || Flattener || RammingMovement || RandomMovement ||
+            MinimumRiskMovement || StopAndGo || ExactPathPredictorMovement) &&
+                (iff(WaveSurfing, TrueSurfing || GoToSurfing )) && !(TrueSurfing && GoToSurfing ) &&
+                implies(Fluid, RandomMovement)  &&
+                implies(Orbital, RandomMovement)&&
+
+        implies(Targeting, TDA594) &&
+            iff(Targeting, VirtualGuns || GuessFactorGun ||HeadOnTargeting || CircularTargeting ||
+            LinearTargeting || PlayItForward || RandomTargeting || PatternMatching || CrowdTargeting) &&
+                implies(KDTreeTargeting, PlayItForward) &&
+
+        implies(Utilities, TDA594) &&
+            iff(Utilities, Segmentation || DynamicClustering || MovementStratChange || TargetingStratChange ||
+            RadarStratChange || EnergyManagement || MatchTypeIdentification) &&
+
+        implies(Radar, TDA594) &&
+            iff(Radar, TurnMultiplier || InfinityLock || SpinRadar) &&
+
+        implies(DataManagement, TDA594) &&
+            iff(DataManagement, SaveTargetingData || SaveMovementData || SaveGuessFactor) &&
+                implies(SaveBulletSynchronizedSnapshots, SaveTargetingData) &&
+                implies(SaveWaveSurfingStats, SaveMovementData);
+
 }
