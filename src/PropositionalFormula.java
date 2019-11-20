8bf35323b58414652628b07742e4feff8de723da
isaksc2 <isaksc@student.chalmers.se>
2019-11-20 10:37:51 +0100

    added some dependencies + variables

---
+++ src/PropositionalFormula.java
@@ -0,0 +1,51 @@
+
+public class PropositionalFormula {
+	boolean TDA594;
+	
+	boolean Movement;
+	boolean WaveSurfing;
+	boolean TrueSurfing;
+	boolean GoToSurfing;
+	boolean PrecisPrediction;
+	boolean Flattener;
+	boolean RammingMovement;
+	boolean RandomMovement;
+	boolean Fluid;
+	boolean Orbital;
+	boolean MinimumRiskMovement;
+	boolean StopAndGo;
+	boolean MovementStratChange;
+	boolean ExactPathPredictorMovement;
+	
+	boolean Targeting;
+	boolean VirtualGuns;
+	boolean GuessFactorGun;
+	boolean HeadOnTargeting;
+	boolean CircularTargeting;
+	boolean LinearTargeting;
+	boolean TargetingStratChange;
+	boolean PlayItForward;
+	boolean KDTreeTargeting;
+	boolean RandomTargeting;
+	boolean PatternMatching;
+	boolean CrowdTargeting;
+	
+	boolean Utilities;
+	boolean Segmentation;
+	boolean DynamicClustering;
+	boolean EnergyManagement;
+	boolean MatchTypeIdentification;
+	
+	boolean Radar;
+	boolean TurnMultiplier;
+	boolean InfinityLock;
+	boolean RadarStratChange;
+	boolean SpinRadar;
+	
+	boolean DataManagement;
+	boolean SaveTargetingData;
+	boolean SaveBulletSynchronizedSnapshots;
+	boolean SaveMovementData;
+	boolean SaveWaveSurfing;
+	boolean SaveGuessFactor;
+}
