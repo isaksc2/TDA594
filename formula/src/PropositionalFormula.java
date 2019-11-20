7c4d90c40603e6a3f9d93c4626ce9311a52ce3db
isaksc2 <isaksc@student.chalmers.se>
2019-11-20 16:07:57 +0100

    fixed virtualguns int + comments

--- formula/src/PropositionalFormula.java
+++ formula/src/PropositionalFormula.java
@@ -1,12 +1,13 @@
 
 public class PropositionalFormula {
 	static boolean TDA594;
-	
+
+	// Movement
 	static boolean Movement;
     static boolean WaveSurfing;
     static boolean TrueSurfing;
     static boolean GoToSurfing;
-    static boolean PrecisPrediction;
+    static boolean PrecisePrediction;
     static boolean Flattener;
     static boolean RammingMovement;
 	static boolean RandomMovement;
@@ -18,8 +19,8 @@ public class PropositionalFormula {
 	static boolean GuessFactorDodging;
 	static boolean OscillatorMovement;
 
+	// Targeting
 	static boolean Targeting;
-	static boolean VirtualGuns;
 	static boolean GuessFactorGun;
 	static boolean HeadOnTargeting;
 	static boolean CircularTargeting;
@@ -30,22 +31,25 @@ public class PropositionalFormula {
 	static boolean PatternMatching;
 	static boolean CrowdTargeting;
 
+	// Utilities
 	static boolean Utilities;
 	static boolean Segmentation;
 	static boolean DynamicClustering;
     static boolean MovementStratChange;
     static boolean TargetingStratChange;
-    static int nbrVirtualGuns;
+    static int nbrVirtualGuns = 0;
     // VirtualGuns = nbrVirtualGuns > 0
     static boolean RadarStratChange;
 	static boolean EnergyManagement;
 	static boolean MatchTypeIdentification;
 
+	// Radar
 	static boolean Radar;
 	static boolean TurnMultiplier;
 	static boolean InfinityLock;
 	static boolean SpinRadar;
 
+	// Data Management
 	static boolean DataManagement;
 	static boolean SaveTargetingData;
 	static boolean SaveBulletSynchronizedSnapshots;
@@ -63,61 +67,77 @@ public class PropositionalFormula {
 
     private static boolean evaluateFormula() {
 
+	    boolean VirtualGuns = nbrVirtualGuns > 0;
+
         boolean formula =
-                // dependencies shown in the model, indentation means 1 level down
-                implies(Movement, TDA594) &&
-                        iff(Movement, WaveSurfing || PrecisPrediction || Flattener || RammingMovement || RandomMovement ||
-                                MinimumRiskMovement || StopAndGo || ExactPathPredictorMovement ||
-                                GuessFactorDodging || OscillatorMovement) &&
-                        (iff(WaveSurfing, TrueSurfing || GoToSurfing )) && !(TrueSurfing && GoToSurfing ) &&
-                        implies(Fluid, RandomMovement)  &&
-                        implies(Orbital, RandomMovement) &&
+            // dependencies shown in the model, indentation means 1 level down
+            // Movement
+            implies(Movement, TDA594) &&
+                iff(Movement, WaveSurfing || PrecisePrediction || Flattener || RammingMovement || RandomMovement ||
+                        MinimumRiskMovement || StopAndGo || ExactPathPredictorMovement ||
+                        GuessFactorDodging || OscillatorMovement) &&
+                (iff(WaveSurfing, TrueSurfing || GoToSurfing )) && !(TrueSurfing && GoToSurfing ) &&
+                implies(Fluid, RandomMovement)  &&
+                implies(Orbital, RandomMovement) &&
 
-                        implies(Targeting, TDA594) &&
-                        iff(Targeting, GuessFactorGun ||HeadOnTargeting || CircularTargeting ||
-                                LinearTargeting || PlayItForward || RandomTargeting || PatternMatching || CrowdTargeting) &&
-                        implies(KDTreeTargeting, PlayItForward) &&
+            // Targeting
+            implies(Targeting, TDA594) &&
+                iff(Targeting, GuessFactorGun ||HeadOnTargeting || CircularTargeting ||
+                        LinearTargeting || PlayItForward || RandomTargeting || PatternMatching || CrowdTargeting) &&
+                implies(KDTreeTargeting, PlayItForward) &&
 
-                        implies(Utilities, TDA594) &&
-                        iff(Utilities, Segmentation || DynamicClustering || MovementStratChange || TargetingStratChange ||
-                                RadarStratChange || EnergyManagement || MatchTypeIdentification) &&
-                        implies(VirtualGuns, TargetingStratChange) &&
+            // Utilities
+            implies(Utilities, TDA594) &&
+                iff(Utilities, Segmentation || DynamicClustering || MovementStratChange || TargetingStratChange ||
+                        RadarStratChange || EnergyManagement || MatchTypeIdentification) &&
+                implies(VirtualGuns, TargetingStratChange) &&
 
-                        implies(Radar, TDA594) &&
-                        iff(Radar, TurnMultiplier || InfinityLock || SpinRadar) &&
+            // Radar
+            implies(Radar, TDA594) &&
+                iff(Radar, TurnMultiplier || InfinityLock || SpinRadar) &&
 
-                        implies(DataManagement, TDA594) &&
-                        iff(DataManagement, SaveTargetingData || SaveMovementData || SaveGuessFactor) &&
-                        implies(SaveBulletSynchronizedSnapshots, SaveTargetingData) &&
-                        implies(SaveWaveSurfingStats, SaveMovementData) &&
+            // Data Management
+            implies(DataManagement, TDA594) &&
+                iff(DataManagement, SaveTargetingData || SaveMovementData || SaveGuessFactor) &&
+                implies(SaveBulletSynchronizedSnapshots, SaveTargetingData) &&
+                implies(SaveWaveSurfingStats, SaveMovementData) &&
 
-                        // cross tree constraints
-                        implies(PrecisPrediction, WaveSurfing) &&
-                        implies(Flattener, WaveSurfing) &&
-                        implies(GuessFactorDodging, WaveSurfing) &&
+            // cross tree constraints
+            // -------------------------------------------
 
-                        implies(Targeting, Radar) &&
+            // Movement
+            implies(PrecisePrediction, WaveSurfing) &&
+            implies(Flattener, WaveSurfing) &&
+            implies(GuessFactorDodging, WaveSurfing) &&
 
-                        implies(Segmentation, WaveSurfing || GuessFactorGun) &&
-                        implies(DynamicClustering, WaveSurfing || GuessFactorGun) &&
-                        implies(MovementStratChange, Movement) &&
-                        implies(TargetingStratChange, Targeting) &&
-                        implies(VirtualGuns, Targeting) &&
-                        implies(RadarStratChange, Radar) &&
-                        implies(EnergyManagement, Targeting) &&
-                        implies(MatchTypeIdentification, Targeting || Movement) &&
+            // Targeting
+            implies(Targeting, Radar) &&
 
-                        implies(SaveTargetingData, Targeting) &&
-                        implies(SaveMovementData, Movement) &&
-                        implies(SaveWaveSurfingStats, WaveSurfing) &&
-                        implies(SaveGuessFactor, GuessFactorGun || GuessFactorDodging);
+            // Utilities
+            implies(Segmentation, WaveSurfing || GuessFactorGun) &&
+            implies(DynamicClustering, WaveSurfing || GuessFactorGun) &&
+            implies(MovementStratChange, Movement) &&
+            implies(TargetingStratChange, Targeting) &&
+            implies(VirtualGuns, Targeting) &&
+            implies(RadarStratChange, Radar) &&
+            implies(EnergyManagement, Targeting) &&
+            implies(MatchTypeIdentification, Targeting || Movement) &&
 
+            // DataManagement
+            implies(SaveTargetingData, Targeting) &&
+            implies(SaveMovementData, Movement) &&
+            implies(SaveWaveSurfingStats, WaveSurfing) &&
+            implies(SaveGuessFactor, GuessFactorGun || GuessFactorDodging);
 
         return formula;
     }
 
 	public static void main(String[] args) {
-        Movement=false;
+	    TDA594 = true;
+        Fluid = true;
+        Movement = true;
+        MinimumRiskMovement = false;
+        RandomMovement = true;
 	    System.out.println(evaluateFormula());
     }
 }
