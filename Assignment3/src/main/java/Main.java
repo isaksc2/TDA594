03299b1162c11f447be8d89182f5aa60cf2a4c21
Johannes <Jogust95@gmail.com>
2019-11-27 11:52:49 +0100

    Som refactoring and added comments
    Extracted counting of nmbr of dead features to a own method

--- Assignment3/src/main/java/Main.java
+++ Assignment3/src/main/java/Main.java
@@ -12,34 +12,22 @@ public class Main {
     static String filename = "small.dimacs";
     static ISolver solver;
     static Reader reader;
-    static WriteToFile writer;
     static List<int[]> implications = new ArrayList<>();
 
     public static void main(String[] args){
         solver = SolverFactory.newDefault();
         solver.setTimeout(3600); // 1 hour timeout
         reader = new DimacsReader(solver);
-        writer = new WriteToFile();
 
         try {
             IProblem problem = reader.parseInstance(Main.class.getClassLoader().getResourceAsStream(filename));
             if (problem.isSatisfiable()) {
                 System.out.println("Problem is satisfiable!");
                 System.out.println("There are " + problem.nVars() + " variables!");
-                int numDeadFeatures = 0;
                 HashMap<Integer, String> names = findNames();
                 // Loop over all variables.
                 System.out.println("Dead features: ");
-                int[] vars = problem.model();
-                for (int var : vars) {
-                    var = Math.abs(var);
-                    int[] a = {var}; // set the current feature to be enabled (true)
-                    IVecInt assumptions = new VecInt(a);
-                    if (!problem.isSatisfiable(assumptions)) {
-                        numDeadFeatures++;
-                        System.out.println(var + " " + names.get(var));
-                    }
-                }
+                int numDeadFeatures = printAndCountDeadFeatures(problem,names);
                 System.out.println("There are " + numDeadFeatures + " dead features!");
                 findImplications(problem, names);
             }
@@ -56,6 +44,25 @@ public class Main {
             System.out.println("Timeout, sorry!");
         }
     }
+    static int printAndCountDeadFeatures(IProblem problem, HashMap<Integer, String> names) {
+        int numDeadFeatures = 0;
+        int[] vars = problem.model();
+        try {
+            for (int var : vars) {
+                var = Math.abs(var);
+                int[] a = {var}; // set the current feature to be enabled (true)
+                IVecInt assumptions = new VecInt(a);
+                if (!problem.isSatisfiable(assumptions)) {
+                    numDeadFeatures++;
+                    System.out.println(var + " " + names.get(var));
+                }
+            }
+        } catch (TimeoutException e) {
+            System.out.println("Timeout, sorry!");
+        }
+        return numDeadFeatures;
+    }
+
 
     static void findImplications(IProblem problem, HashMap<Integer, String > nameMap) throws FileNotFoundException, ParseFormatException,
             IOException, ContradictionException, TimeoutException{
@@ -65,59 +72,36 @@ public class Main {
             for(int iVar2=vars.indexOf(var1); iVar2<vars.size(); iVar2++){
                 int a = var1;
                 int b = vars.get(iVar2);
-                IVecInt testassump = new VecInt();
-                testassump.push(a);
-                testassump.push(-b);
-                Boolean test = problem.isSatisfiable(testassump);
-                if(test){
+                //Add a and -b to an vecInt
+                IVecInt bImpavec = new VecInt();
+                bImpavec.push(a);
+                bImpavec.push(-b);
+
+                //Check if b implies a and if it does adds it to a list with pairs of implications
+                Boolean bImpa = problem.isSatisfiable(bImpavec);
+                if(bImpa){
                     int[] pair = {b,a };
                     implications.add(pair);
                 }
-                IVecInt testassump2 = new VecInt();
-                testassump2.push(-a);
-                testassump2.push(b);
-                Boolean test2 = problem.isSatisfiable(testassump);
-                if(test2){
+
+                //Add b and -a to an vecInt
+                IVecInt aImpbvec = new VecInt();
+                aImpbvec.push(-a);
+                aImpbvec.push(b);
+
+                //Check if a implies b and if it does adds it to a list with pairs of implications
+                Boolean aImpb = problem.isSatisfiable(aImpbvec);
+                if(aImpb){
                     int[] pair2 = {a, b};
                     implications.add(pair2);
                 }
-                /*
-                int[] a = {var1, vars.get(iVar2)};    // SS = S
-                IVecInt assumptions = new VecInt(a);
-                boolean ss = problem.isSatisfiable(assumptions);
-                a[1] = -a[1];          // SF = S
-                assumptions = new VecInt(a);
-                boolean sf = problem.isSatisfiable(assumptions);
-                a[0] = -a[0];      // FF = S
-                assumptions = new VecInt(a);
-                boolean ff = problem.isSatisfiable(assumptions);
-                a[1] = -a[1];         // FS = F
-                assumptions = new VecInt(a);
-                boolean fs = problem.isSatisfiable(assumptions);
-                if(ss && fs && ff && !sf){
-                    int[] pair = {a[0], -a[1]};
-                    implications.add(pair);
-                }
-                if(ss && sf && ff && !fs){
-                    int[] pair = {-a[1], a[0]};
-                    implications.add(pair);
-                }*/
             }
         }
         System.out.println(implications.size());
-        writeImplicationsToFile(nameMap);
+        // Comment out this for faster calculation, writing to file takes time.
+        WriteToFile.writeImplicationsToFile(implications, nameMap);
+    }
 
-    }
-    static void writeImplicationsToFile(HashMap<Integer, String > nameMap){
-        StringBuilder stringToWrite = new StringBuilder();
-        Iterator it = implications.iterator();
-        while(it.hasNext()) {
-            int[] item = (int[]) it.next();
-            stringToWrite.append("{" + nameMap.get(item[0]) + " ==> " + nameMap.get(item[1]) + "}" + "\n");
-            writer.WriteText(stringToWrite.toString());
-        }
-        System.out.println("Implications written");
-    }
 
     static  HashMap<Integer, String> findNames() {
         HashMap<Integer, String> names = new HashMap<Integer, String>();
--- Assignment3/src/main/java/WriteToFile.java
+++ Assignment3/src/main/java/WriteToFile.java
@@ -4,8 +4,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class WriteToFile {
-        void WriteText(String text){
+    /*
+    Iterate through the list of implications and mapping the int to corresponding
+    name and send it as a string to the WriteText function.
+    */
+    static void writeImplicationsToFile(List implications, HashMap<Integer, String > nameMap){
+        StringBuilder stringToWrite = new StringBuilder();
+        Iterator it = implications.iterator();
+        while(it.hasNext()) {
+            int[] item = (int[]) it.next();
+            stringToWrite.append("{" + nameMap.get(item[0]) + " ==> " + nameMap.get(item[1]) + "}" + "\n");
+            WriteText(stringToWrite.toString());
+        }
+        System.out.println("Implications written");
+    }
+        /*
+        Creates a file if there is none on desktop with name implications.txt
+        writes the string which is given to that file
+        When restarting the program it writes over the old content.
+        */
+        static void WriteText(String text){
             try {
                 String userHomeFolder = System.getProperty("user.home");
                 File statText = new File(userHomeFolder + "/desktop/implications.txt");
